4ab2032cd5897e3e77474f5861efeae45d6b4abf
SOFIA Triaca <108841209+SofiaTriaca@users.noreply.github.com>
2022-08-30 21:50:50 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
+const express = require ("express");
+const app = express ();
+const path = require("path");
+app.use(express.static("public"));
 
-const express = require('express');
-const app = express();
+/* app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/test.html"))
+});
+app.get("/home", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+}); */
 
-app.use(express.static('public'));
-
-const path = require('path');
-
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/home.html'));
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
-app.post('/', (req,res)=>{
-    console.log("se realizó el registro");
-    res.sendFile(path.join(__dirname,'./views/home.html'));
+app.get("/register", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/register.html"))
 });
-app.get('/registro', (req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/registrar.html'));
-});
-app.get('/login', (req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/login.html'));
+app.post("/register", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/register.html"))
 });
 
-// Se levanta el servidor
-app.listen( 3000 , ()=>{
-    console.log('Servidor levantado en el puerto 3000');
+app.get("/login", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
+
+app.post("/login", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Servidor corriendo")
 });
